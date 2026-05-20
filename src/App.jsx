830141f36d65f8602723b698a7f76f27c877cf80
SRI VIKAS S R <srivikasramadas@gmail.com>
2026-05-20 19:01:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="dashboard">
+
+        <header className="header">
+          <h1>Smart Building Dashboard</h1>
+          <p>Admin Panel</p>
+        </header>
+
+        <main className="dashboard-container">
+          <p>widgets will go here</p>
+        </main>
+
+      </div>
+    </>
+  )
+}
+
+export default App
